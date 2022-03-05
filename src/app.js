2572e790ff071cbfc22a53f076a7239bd1ab6da9
Andrew Barker <adbarker@sky.com>
2022-03-05 15:23:22 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const app = express()
+const port = process.env.PORT || 3001
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// set up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// set up static dir to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Swirlacre Photography',
+        name: 'Andrew Barker'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Andrew Barker'
+    })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contact',
+        contacttext: 'Contact Details',
+        name: 'Andrew Barker'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404-page', {
+        title: 'Help Page not found',
+        message: 'Help Page not found - go to home page',
+        name: 'Andrew Barker'
+    })
+})
+
+// match anything not matched so far
+app.get('*', (req, res) => {
+    res.render('404-page', {
+        title: 'Page not found!',
+        message: 'Page not found - go to home page',
+        name: 'Andrew Barker'
+    })
+})
+
+// start the server
+app.listen(port, () => {
+    console.log('Server is running on port ' + port)
+})
